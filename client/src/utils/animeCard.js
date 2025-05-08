0fd66f7b0d4e0efb5bce9d2a3133f866eed1e774
Johnny Michael Alves Faria <johnny.faria1992@gmail.com>
2025-05-08 07:58:16 +0000

    correção de rota

--- client/src/utils/animeCard.js
+++ client/src/utils/animeCard.js
@@ -3,15 +3,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./animeCard.css";
 
-function AnimeCard({ name, image, episodes, minDigits }) {
+function AnimeCard({ id, name, image, episodes, minDigits }) {
   const navigate = useNavigate();
 
   const handleClick = () => {
     // Salvar nome, episódios e minDigits no sessionStorage
     sessionStorage.setItem("anime", JSON.stringify({ name, episodes, minDigits }));
 
-    // Navegar para a página de episódios com o nome do anime na URL
-    navigate(`/episodes/${encodeURIComponent(name)}`);
+    // Navegar para a página de episódios com o id do anime na URL, não o nome
+    navigate(`/episodes`);
   };
 
   return (
